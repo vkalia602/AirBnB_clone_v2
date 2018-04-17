2cc056aad97882b196d8b3e58faa111ff114fead
vkalia602 <vkalia602@gmail.com>
2018-04-17 21:41:12 +0000

    Added things

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
+"""
+Module for Flask- web framework
+"""
+
+
 from flask import Flask
 app = Flask(__name__)
-app.debug = True
+
+
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def things(text=None):
     return "C " + text.replace("_", " ")
 
+
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text=''):
-    if text is '':
-        text = "is cool"
+def python(text="is cool"):
     return "Python " + text.replace("_", " ")
 
 if __name__ == '__main__':
