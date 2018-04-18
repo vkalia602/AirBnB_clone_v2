f107b77f3aec16fc48c0046a9427866d2c7c4ca3
vkalia602 <vkalia602@gmail.com>
2018-04-18 03:15:33 +0000

    remove initial value of n

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -36,7 +36,7 @@ def number(n=None):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n=None):
+def number_template(n):
     return render_template('5-number.html', n=n)
 
 
